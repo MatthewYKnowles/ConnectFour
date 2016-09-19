2873fb18a19c288b679967b7e19c4e0ab49ff950
Matthew Knowles <matthewyknowles@gmail.com>
2016-09-18 20:36:22 -0400

    added dropping second piece in column

--- app/gameboard/gameboard.component.spec.ts
+++ app/gameboard/gameboard.component.spec.ts
@@ -4,12 +4,13 @@ describe("gameboard", ()=> {
     let gameBoard: GameboardComponent = new GameboardComponent();
     spyOn(gameBoard, 'drawPiece');
     gameBoard.dropInColumn(1);
-    expect(gameBoard.drawPiece).toHaveBeenCalledWith(1, 7);
+    expect(gameBoard.drawPiece).toHaveBeenCalledWith(1, 6);
   });
   it("should drop a second piece in column 1 and draw it in row 6", ()=> {
     let gameBoard: GameboardComponent = new GameboardComponent();
     spyOn(gameBoard, 'drawPiece');
     gameBoard.dropInColumn(1);
-    expect(gameBoard.drawPiece).toHaveBeenCalledWith(1, 7);
+    gameBoard.dropInColumn(1);
+    expect(gameBoard.drawPiece).toHaveBeenCalledWith(1, 5);
   });
 });
--- app/gameboard/gameboard.component.ts
+++ app/gameboard/gameboard.component.ts
@@ -9,21 +9,14 @@ export class GameboardComponent implements AfterViewInit {
 
   context: CanvasRenderingContext2D;
   playersTurn: string = "red";
+  grid: any = {1: [".",".",".",".",".",".","."], 2: [".",".",".",".",".",".","."], 3: [".",".",".",".",".",".","."], 4: [".",".",".",".",".",".","."], 5: [".",".",".",".",".",".","."], 6: [".",".",".",".",".",".","."]};
 
   @ViewChild("myCanvas") myCanvas: any;
 
   ngAfterViewInit(): void {
-    console.log(this.myCanvas);
     this.context = this.myCanvas.nativeElement.getContext("2d");
     this.dropInColumn(1);
-    this.drawPiece(2, 7);
-    this.drawPiece(1, 6);
-    this.drawPiece(1, 5);
-    this.drawPiece(2, 6);
-    this.drawPiece(3, 7);
-    this.drawPiece(3, 6);
-    this.drawPiece(4, 7);
-    this.drawPiece(4, 6);
+    this.dropInColumn(1);
   }
 
   drawPiece(column: number, row: number) {
@@ -31,20 +24,29 @@ export class GameboardComponent implements AfterViewInit {
     let radius = 45;
     let startAngle = 0;
     let finishAngle = 2 * Math.PI;
-    let yAxisCenterPoint = (row - 1) * 100 - 50;
+    let yAxisCenterPoint = (row) * 100 - 50;
     let xAxisCenterPoint = column * 100 - 50;
     ctx.beginPath();
     ctx.arc(xAxisCenterPoint, yAxisCenterPoint, radius, startAngle, finishAngle);
     ctx.fillStyle = this.playersTurn;
     ctx.fill();
-    this.changeTurn();
   }
 
   dropInColumn(column: number) {
-    let row = 7;
+    let row = 6;
+    if (this.grid[row][column - 1] !== ".") {
+      row--;
+    }
+    this.trackPieceInGrid(column, row);
     this.drawPiece(column, row);
+    this.changeTurn();
+    console.log(this.grid);
   }
   changeTurn(): void {
     this.playersTurn = this.playersTurn === "red" ? "black": "red";
+  }
+
+  private trackPieceInGrid(column: number, row: number) {
+    this.grid[row][column - 1] = this.playersTurn[0];
   }
 }
